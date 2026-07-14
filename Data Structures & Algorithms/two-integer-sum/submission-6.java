15eb1a21b90f5ca87f9daa60efd518c5ffb14253
CrabbRex <66297372+CrabbRex@users.noreply.github.com>
2026-07-14 14:49:15 +1000

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] toReturn = new int[2];
+        for(int i=0; i<nums.length; i++) {
+            for(int j=1; j<nums.length; j++) {
+                if(nums[i] + nums[j] == target && i != j) {
+                    toReturn[0] = i;
+                    toReturn[1] = j;
+                    if(j < i) {
+                        toReturn[0] = j;
+                        toReturn[1] = i;
+                    }
+                }
+            }
+        }
+        return toReturn;
+    }
+}
